df8b7b33cb9bb692aa2660fa6070a5a7a52d9a4c
NourOumy <ryahi.nourelhouda93@gmail.com>
2024-02-05 15:42:29 +0100

    initialisation

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,53 @@
-// Créer l'input pour prendre ce que vous voulez ajouter à la liste des courses
-// Prévoir un élément HTML qui va recevoir les éléments que vous allez ajouter à votre liste des courses
-// Au click, quand vous "ajoutez" un élément à votre liste, il apparaît dans la liste et le champ input se ré-initialise
-// Vous ajouterez et rangerez ces éléments dans un joli tableau : )
-// et c'est en faisant une boucle sur ce joli tableau que vous allez re-générer à chaque fois la liste des courses mise à jour : )
+// variables
 
+let carsDiv = document.querySelector(".cars"),
+    allcar = document.querySelectorAll(".car"),
+    winner = document.querySelector(".winner"),
+    testdrive = winner.querySelector ('img'),
+    compteur = document.querySelector('.compteur'),
+    decompte = 5,
+    body = document.querySelector("body"),
+    wrapper = document.querySelector(".wrapper")
+    titles = document.querySelector(".titles")
 
+// event click pas gagnant
 
-let toadd = document.querySelector(".text")
-let submit = document.querySelector(".submit")
-let liste = document.querySelector(".liste")
-let nothing = document.querySelector(".nothing")
+    allcar.forEach(function(car){
+    car.addEventListener("click",function(){
+        decompte --
 
-function addtolist (){
-    nothing.innerHTML= "A acheter :"
-    liste.innerHTML+=`
-<div><p>${toadd.value}</p><span class="delete" href="#">❌</span> </div>
-`
-toadd.value= ""
+        let tryImg = car.querySelector('img')
+
+        tryImg.src = "images/try.jpeg"
+
+        compteur.innerHTML=decompte
+        if(decompte==0){
+            wrapper.style.display= "none"
+            body.innerHTML += `<h1>Vous avez épuisé toutes vos chances</h1>`   
+        }
+})
+})
+
+function dispnone(element){
+    element.style.display ="none"
 }
 
-submit.addEventListener("click", function(){
-    addtolist()
-    let allspans = document.querySelectorAll("span")
+// event click gagnant
 
-allspans.forEach(function(allspan){
-    allspan.addEventListener("click", function(){
-        allspan.parentElement.remove()
-        allspans = document.querySelectorAll("span");
-
-          if (allspans.length ==0) {
-            nothing.innerHTML=" Il n'y a rien dans votre list pour le moment"
-
-          }
+winner.addEventListener("click",function(){
+    
+    testdrive.src = "images/testdrive.jpg"
+    allcar.forEach(function(car){
+        dispnone(car)
+        // car.style.display= "none"
     })
+    carsDiv.style.display="inline"
+    dispnone(titles)
+    // titles.style.display="none"
+    winner.style.width= "400px"
+    testdrive.style.width="400px"
+    body.innerHTML+=`
+    <h1>Vous avez gagné !</h1>`
 })
-}
-)
 
-// submit.addEventListener("keydown", function(event){
-//     if (event.keyCode === 13) {
-//         addtolist()
-//     }
-// })
-
-toadd.addEventListener("keyup", function(event) {
-    // event.preventDefault();
-    if (event.key === "Enter") {
-        submit.click();
-    }
-})
 
